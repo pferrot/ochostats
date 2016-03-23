44b1d63ac2aed21ccce45dc4d3f15e8e97a895c8
Patrice Ferrot <patrice.ferrot@autodesk.com>
2016-03-23 14:42:04 +0100

    Version 0.3
    - All subprocesses (Apache2, Grafana, StatsD, Carbon and PostgreSQL) are
    now monitored by Ochopod
    - Using template file for grafana.ini: this allows to pass dynamic
    Grafana configuration when deploying Ochostats (e.g. Grafana root URL)

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ Ochostats provides a ready to use <a href="https://github.com/autodesk-cloud/och
 
 You have two options to use Ochostats:
 <ol>
-<li>Either use the base image (pferrot/ochostats:1.0.6_20151029154900CET_0.2) as is and e.g. customize dashboards from grafana UI once the container is up and running.</li>
+<li>Either use the base image (pferrot/ochostats:1.0.6_20151029154900CET_0.3) as is and e.g. customize dashboards from grafana UI once the container is up and running.</li>
 <li>Or (recommended option) create your own Docker image by extending the Ochostats base image and include whatever customization you need (e.g. dashboards) directly within your base image. See the example under images/extend and follow the instructions below.</li>
 </ol>
 
@@ -17,7 +17,10 @@ Use the files under images/extend as template and adapt to your needs as follows
   - Replace 'my_custom_salty_string' with your own random unique secret key for Graphite
   - *Optional*: Configure the Django timezone (search for 'Set your local timezone' in Dockerfile) - defaults to 'America/Los_Angeles'
 - *Optional*: configure schemas definitions and aggregation methods for Whisper files by modifying the files under images/extend/resources/carbon
-- *Optional*: customize Grafana (e.g. enable anonymous access) by editing the file images/extend/resources/grafana_init/grafana.ini
+- *Optional*: customize Grafana (e.g. enable anonymous access) by editing *both* images/extend/resources/grafana_init/grafana.ini and 
+  images/extend/resources/grafana_init/templates/grafana.ini. The first one is used for the creation of the Docker image and the second one 
+  is the template that is used to create the actual grafana.ini file used by Grafana at runtime. The template allows to pass some dynamic configuration to Grafana (e.g. its root URL),
+  which is convenient for reusing the same image in different contexts (e.g. dev, staging, production,...)
 - *Optional*: customize StatsD by editing the file images/extend/resources/statsd/config.js
 - *Optional*: modify the default Grafana admin password by editing the file images/extend/resources/grafana_init/change_admin_password.json
 - *Optional*: modify the Grafana main org name by editing the file images/extend/resources/grafana_init/update_org_name.json
@@ -36,6 +39,10 @@ Regardless of the option that you choose (using the base image directly or creat
 - Access the Grafana UI at http://&lt;ochostats_host&gt;:3000 (get the IP/host thanks to Ochothon or the Marathon UI) and start monitoring and building beautiful graphs
   
 ## Release notes
+
+### 0.3 (3/23/2016)
+- All subprocesses (Apache2, Grafana, StatsD, Carbon and PostgreSQL) are now monitored by Ochopod
+- Using template file for grafana.ini: this allows to pass dynamic Grafana configuration when deploying Ochostats (e.g. Grafana root URL)
 
 ### 0.2 (1/13/2016)
 - Existing base image (pferrot/ochostats:1.0.6_20151029154900CET_0.2) with instructions on how to extend it
--- images/ochostats/resources/pod/pod.py
+++ images/ochostats/resources/pod/pod.py
@@ -14,46 +14,148 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+import json
+import os
 import logging
 import time
 
+from jinja2 import Environment, FileSystemLoader
 from ochopod.bindings.generic.marathon import Pod
 from ochopod.models.piped import Actor as Piped
+from ochopod.core.utils import shell
 
 logger = logging.getLogger('ochopod')
 
 
 if __name__ == '__main__':
+    
+    cfg = json.loads(os.environ['pod'])
 
     class Strategy(Piped):
 
-        cwd = '/opt/ochostats'
+        cwd = '/usr/share/grafana'
         
         check_every = 60.0
         
         pipe_subprocess = True
 
-        pid = None
+        pid_grafana = None
+        pid_apache2 = None
+        pid_statsd = None
+        pid_carbon = None
+        pid_postgres = None
 
-        since = 0.0
+        since_grafana = 0.0
+        since_apache2 = 0.0
+        since_statsd = 0.0
+        since_carbon = 0.0
+        since_postgres = 0.0   
+        
+        def get_pid(self, hints, hints_ignore = None):
+            if hints_ignore:
+                hints_ignore.append("grep")
+            else:
+                hints_ignore = ["grep"]
+            assert hints, "need at least one hint"
+            try:
+                _, lines = shell("ps -ef | grep -v %s | grep %s | awk '{print $2}'" % (" | grep -v ".join(hints_ignore),  " | grep ".join(hints)))
+                if lines:
+                    return [int(x) for x in lines]
+                else:
+                    return None
+            except Exception:
+                return None
+            
+        
 
         def sanity_check(self, pid):
+            
 
             #
             # - simply use the provided process ID to start counting time
             # - this is a cheap way to measure the sub-process up-time
             #
             now = time.time()
-            if pid != self.pid:
-                self.pid = pid
-                self.since = now
+            if pid != self.pid_grafana:
+                self.pid_grafana = pid
+                self.since_grafana = now
+             
+            pid_apache2 = self.get_pid(hints = ["apache2ctl"])
+            assert pid_apache2, "Apache2 process not running"
+            assert len(pid_apache2) == 1, "More than 1 Apache2 process"
+            if pid_apache2[0] != self.pid_apache2:
+                self.pid_apache2 = pid_apache2[0]
+                self.since_apache2 = now
+            
+            
+            pid_statsd = self.get_pid(hints = ["statsd"])
+            assert pid_statsd, "StatsD process not running"
+            assert len(pid_statsd) == 1, "More than 1 StatsD process"
+            if pid_statsd[0] != self.pid_statsd:
+                self.pid_statsd = pid_statsd[0]
+                self.since_statsd = now
+            
+            pid_carbon = self.get_pid(hints = ["carbon"])
+            assert pid_carbon, "Carbon process not running"
+            assert len(pid_carbon) == 1, "More than 1 Carbon process"
+            if pid_carbon[0] != self.pid_carbon:
+                self.pid_carbon = pid_carbon[0]
+                self.since_carbon = now
+            
+            pid_postgres = self.get_pid(hints = ["postgresql", "sudo"])
+            assert pid_postgres, "PostgreSQL process not running"
+            assert len(pid_postgres) == 1, "More than 1 PostgreSQL process"
+            if pid_postgres[0] != self.pid_postgres:
+                self.pid_postgres = pid_postgres[0]
+                self.since_postgres = now
+        
+            
+            lapse_grafana = (now - self.since_grafana) / 3600.0
+            lapse_apache2 = (now - self.since_apache2) / 3600.0
+            lapse_statsd = (now - self.since_statsd) / 3600.0
+            lapse_carbon = (now - self.since_carbon) / 3600.0
+            lapse_postgres = (now - self.since_postgres) / 3600.0
+            
+            # The global uptime is the min of all uptimes.
+            lapse = min(lapse_grafana, lapse_apache2, lapse_statsd, lapse_carbon, lapse_postgres)
 
-            lapse = (now - self.since) / 3600.0
+            return {'uptime': '%.2f hours' % lapse, 
+                    'uptime Grafana': '%.2f hours (pid %s)' % (lapse_grafana, self.pid_grafana), 
+                    'uptime Apache2': '%.2f hours (pid %s)' % (lapse_apache2, self.pid_apache2), 
+                    'uptime StatsD': '%.2f hours (pid %s)' % (lapse_statsd, self.pid_statsd), 
+                    'uptime Carbon': '%.2f hours (pid %s)' % (lapse_carbon, self.pid_carbon), 
+                    'uptime PostgreSQL': '%.2f hours (pid %s)' % (lapse_postgres, self.pid_postgres)}
+        
+        # Lets make sure that Apache2, StatsD, Carbon and PostgreSQL are running
+        # before starting Grafana.
+        def can_configure(self, js):
+            pid_apache2 = self.get_pid(hints = ["apache2ctl"])
+            assert pid_apache2, "Apache2 process not running"
+            assert len(pid_apache2) == 1, "More than 1 Apache2 process"            
+            
+            pid_statsd = self.get_pid(hints = ["statsd"])
+            assert pid_statsd, "StatsD process not running"
+            assert len(pid_statsd) == 1, "More than 1 StatsD process"
+            
+            pid_carbon = self.get_pid(hints = ["carbon"])
+            assert pid_carbon, "Carbon process not running"
+            assert len(pid_carbon) == 1, "More than 1 Carbon process"
+            
+            pid_postgres = self.get_pid(hints = ["postgresql", "sudo"])
+            assert pid_postgres, "PostgreSQL process not running"
+            assert len(pid_postgres) == 1, "More than 1 PostgreSQL process"
+            
+            logger.debug("Apache2, StatsD, Carbon and PostgreSQL are running - ready to start Grafana")
 
-            return {'uptime': '%.2f hours (pid %s)' % (lapse, pid)}
+        def configure(self, _):            
+            # Defaults to empty dict if key not present.
+            mappings = {'server': cfg['grafana_server'] if 'grafana_server' in cfg.keys() else {}}
+            
+            env = Environment(loader=FileSystemLoader("/opt/ochostats/templates"))       
+            template = env.get_template('grafana.ini')
+            with open('/etc/grafana/grafana.ini', 'w') as f:
+                f.write(template.render(mappings))       
 
-        def configure(self, _):
-
-            return '/usr/bin/nodejs /opt/statsd/stats.js /etc/statsd/localConfig.js', {}
+            return '/usr/sbin/grafana-server --pidfile=/var/run/grafana-server.pid --config=/etc/grafana/grafana.ini cfg:default.paths.data=/var/lib/grafana cfg:default.paths.logs=/var/log/grafana', {}
 
     Pod().boot(Strategy)
